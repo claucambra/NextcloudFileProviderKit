ad4bdf5ff409e03b199faef6257fa9eda3da5f42
Claudio Cambra <developer@claudiocambra.com>
2025-05-19 17:09:00 +0800

    Do not pass in NSFileProviderPage default pages as NC pagination tokens
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Sources/NextcloudFileProviderKit/Enumeration/Enumerator+SyncEngine.swift
+++ Sources/NextcloudFileProviderKit/Enumeration/Enumerator+SyncEngine.swift
@@ -323,7 +323,7 @@ extension Enumerator {
 
     static func readServerUrl(
         _ serverUrl: String,
-        pageSettings: (page: NSFileProviderPage, index: Int, size: Int)? = nil,
+        pageSettings: (page: NSFileProviderPage?, index: Int, size: Int)? = nil,
         account: Account,
         remoteInterface: RemoteInterface,
         dbManager: FilesDatabaseManager,
